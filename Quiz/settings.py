749ae4d98abf79877b4e04c186e739c8d12c56c8
a1exdandy <darkjedi.alex@yandex.ru>
2016-06-09 21:37:24 +0500

    fix settings

--- Quiz/settings.py
+++ Quiz/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 """
 
 import os
-import Quiz.local_settings
+from Quiz.local_settings import *
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -114,3 +114,7 @@ USE_L10N = True
 
 USE_TZ = True
 
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+
+STATIC_URL = '/static/'
